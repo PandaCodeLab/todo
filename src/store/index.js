0bd4a9a152dd0a04729099957baee2d2bff32f0e
StarBurst <panda.code.lab@gmail.com>
2021-07-08 12:58:35 +0300

    beforeDecomp

--- src/store/index.js
+++ src/store/index.js
@@ -1,57 +1,13 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
+import firebase from 'firebase/app'
 
 Vue.use(Vuex)
 
 export default new Vuex.Store({
-  state: {
-    notes: [
-      {
-        id: 1,
-        title: 'Заголовок 1',
-        todos: [
-          {
-            id: 0,
-            text: 'Сделать 1',
-            checked: false
-          },
-          {
-            id: 1,
-            text: 'Сделать 2',
-            checked: false
-          },
-          {
-            id: 2,
-            text: 'Сделать 3',
-            checked: false
-          }
-        ]
-      },
-      {
-        id: 2,
-        title: 'Заголовок 2',
-        todos: [
-          {
-            id: 0,
-            text: 'Сделать 1',
-            checked: false
-          },
-          {
-            id: 1,
-            text: 'Сделать 2',
-            checked: false
-          },
-          {
-            id: 2,
-            text: 'Сделать 3',
-            checked: false
-          }
-        ]
-      }
-    ]
-  },
+  state: {},
   mutations: {
-    CREATE_NOTE: (state, note) => {
+    /*     CREATE_NOTE: (state, note) => {
       state.notes.push(note)
     },
     UPDATE_NOTE: (state, note) => {
@@ -62,15 +18,82 @@ export default new Vuex.Store({
     },
     DELETE_NOTE: (state, noteId) => {
       state.notes = state.notes.filter(n => n.id !== noteId)
-    }
+    } */
   },
   actions: {
-    createNote: ({ commit }, note) => commit('CREATE_NOTE', note),
-    updateNote: ({ commit }, note) => commit('UPDATE_NOTE', note),
-    deleteNote: ({ commit }, noteId) => commit('DELETE_NOTE', noteId)
+    async fetchNotes({ commit }) {
+      try {
+        const notes =
+          (
+            await firebase
+              .database()
+              .ref(`/notes`)
+              .once('value')
+          ).val() || {}
+
+        return Object.keys(notes).map(key => ({ ...notes[key], id: key }))
+      } catch (error) {
+        throw error
+      }
+    },
+
+    async fetchNoteById({ commit }, id) {
+      try {
+        const note =
+          (
+            await firebase
+              .database()
+              .ref(`/notes`)
+              .child(id)
+              .once('value')
+          ).val() || {}
+
+        return { ...note, id }
+      } catch (error) {
+        throw error
+      }
+    },
+
+    async createNote({ commit }, note) {
+      try {
+        const note = await firebase
+          .database()
+          .ref(`/notes`)
+          .push(note)
+
+        return {...note}
+      } catch (error) {
+        throw error
+      }
+    },
+
+    async updateNote({ commit }, note) {
+      try {
+        return await firebase
+          .database()
+          .ref(`/notes`)
+          .child(note.id)
+          .update({ ...note })
+      } catch (e) {
+        commit('setError', e)
+        throw e
+      }
+    },
+
+    async deleteNote({ commit }, id) {
+      try {
+        await firebase
+          .database()
+          .ref(`/notes`)
+          .child(id)
+          .remove()
+      } catch (error) {
+        throw error
+      }
+    }
   },
   getters: {
-    findNoteById: state => id => state.notes.find(n => n.id === id),
+    /*     findNoteById: state => id => state.notes.find(n => n.id === id),
     getNoteMaxId: state => {
       if (state.notes.length > 0) {
         const item = state.notes.reduce((prev, current) =>
@@ -80,7 +103,7 @@ export default new Vuex.Store({
       } else {
         return 0
       }
-    }
+    } */
   },
   modules: {}
 })
